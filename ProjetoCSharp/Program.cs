b2422a33d690b9cb0cb5d5d894699c6d32c4e781
guilherme <guilhermevianalourenco2@gmail.com>
2025-12-30 23:02:50 -0300

    Exercício: verificação de número par ou ímpar

--- ProjetoCSharp/Program.cs
+++ ProjetoCSharp/Program.cs
@@ -6,22 +6,17 @@ namespace ProjetoCSharp {
 
         static void Main(string[] args) {
 
-            Console.WriteLine("Entre com o seu nome completo");
-            string name = Console.ReadLine();
+            Console.WriteLine("Entre com numero inteiro:");
+            int number1 = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("Quantos quartos tem na sua casa?");
-            int q_quartos = int.Parse(Console.ReadLine());
-
-            Console.WriteLine("entre com o preço de um produto:");
-            float preco = float.Parse(Console.ReadLine());
-
-            Console.WriteLine("Entre com o seu último nome, idade e altura (mesma linha):");
-            string[] dados = Console.ReadLine().Split(' ');
-
-            string ultimoNome = dados[0];
-            int idade = int.Parse(dados[1]);
-            float altura = float.Parse(dados[2], CultureInfo.InvariantCulture);
-
+            if (number1 % 2 == 0) 
+               {
+               Console.WriteLine("O número é par");
+               }
+            else 
+               {
+               Console.WriteLine("O número é ímpar");
+               };
         }
     }
 }
