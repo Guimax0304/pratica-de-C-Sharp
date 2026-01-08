61f6ea7e82053ed621cb15ef5d2bd877d419908e
guilherme <guilhermevianalourenco2@gmail.com>
2026-01-08 01:43:37 -0300

    Exercicio de encadeamento com if/ else

--- ProjetoCSharp/Program.cs
+++ ProjetoCSharp/Program.cs
@@ -1,22 +1,28 @@
 ﻿using System;
 using System.Globalization;
+using System.Security.AccessControl;
 
 namespace ProjetoCSharp {
     internal class Program {
 
         static void Main(string[] args) {
 
-            Console.WriteLine("Entre com numero inteiro:");
-            int number1 = int.Parse(Console.ReadLine());
 
-            if (number1 % 2 == 0) 
-               {
-               Console.WriteLine("O número é par");
-               }
-            else 
-               {
-               Console.WriteLine("O número é ímpar");
-               };
+            // Exercicio: encadeamento
+            Console.WriteLine("Digite a hora atual em numero inteiro: ");
+
+            int hour = int.Parse(Console.ReadLine());
+
+
+                if (hour < 12) {
+                    Console.WriteLine("Bom dia!");
+                }
+                else if (hour < 18) {
+                    Console.WriteLine("Boa tarde!");
+                }
+                else {
+                    Console.WriteLine("Boa noite!");
+                }
         }
     }
 }
--- ProjetoCSharp/obj/Debug/net8.0/ProjetoCSharp.AssemblyInfo.cs
+++ ProjetoCSharp/obj/Debug/net8.0/ProjetoCSharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetoCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c95a991e1006b8e444f207d373adf956ad0abebf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2422a33d690b9cb0cb5d5d894699c6d32c4e781")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetoCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetoCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
